dc40610be03d180cfe21d8950b859c51391d6406
nadjayanna <nadjayannabr@gmail.com>
2019-01-03 23:16:40 +0100

    feat: add Dashboard to the App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import Dashboard from './Dashboard'
+import { handlePosts } from '../actions/posts';
+import { connect } from 'react-redux'
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(handlePosts());
+  }
   render() {
+
+    const { loading } = this.props;
+
+    if (loading === null) {
+      return (<div>Connecting...</div>);
+    }
+
     return (
       <div className="app">
         <header className="app-header">
@@ -10,9 +24,18 @@ class App extends Component {
             Your favorite Tech Blog
           </p>
         </header>
+        {loading 
+          ? (<div>Loading...</div>) 
+          : <Dashboard/>
+        }
+        
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  loading: state.posts.loading,
+});
+
+export default connect(mapStateToProps)(App);
