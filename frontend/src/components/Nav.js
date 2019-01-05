822bf16e98f0f75146ced4e4a9b52b2665216271
nadjayanna <nadjayannabr@gmail.com>
2019-01-05 20:04:39 +0100

    feat: create navegation component

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
+import { handleCategories } from '../actions/categories';
+
+
+class Nav extends Component {
+
+  componentDidMount() {
+    this.props.getCategories();
+  }
+
+  render() {
+
+    const { categories, error, loading } = this.props;
+
+    if (error) {
+      return (<div>Error! {error.message}</div>);
+    }
+
+    if(loading) {
+      return (<div>loading</div>)
+    } else {
+      return (
+        <nav className='navbar' role="navigation" aria-label="main navigation">
+            <Link to='/'>all</Link>
+            { 
+              categories.map(categorie => {
+                  return (<Link to={categorie.path}>{categorie.name}</Link>)
+                }
+              )
+            }
+        </nav>
+      )
+    }  
+  }
+}
+
+
+const mapStateToProps = state => ({
+  categories: state.categories.categories,
+  error: state.categories.error,
+  loading: state.categories.loading
+});
+
+const mapDispatchToProps = dispatch => ({
+  getCategories: () => dispatch(handleCategories()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav)
