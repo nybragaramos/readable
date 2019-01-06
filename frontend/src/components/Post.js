bb3ff925ac6f3fd122c031800d3becafb71c77bc
nadjayanna <nadjayannabr@gmail.com>
2019-01-06 20:54:38 +0100

    feat: add layout to post component

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,22 +1,33 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, } from 'react'
+import { FaThumbsUp, FaRegThumbsUp, FaThumbsDown, FaRegThumbsDown, FaRegUser, FaRegComment, FaRegCommentDots } from 'react-icons/fa';
 
 class Post extends Component {
   render() {
 
-    const { post, category } = this.props;
+    const { post } = this.props;
     return (
-      <article>
-        {category 
-          ? (<h2>{post.title}</h2>)
-          : (<Fragment><h2>{post.category}</h2><h3>{post.title}</h3></Fragment>)
-        }        
-        <p>{post.body}</p>
-        <p>{post.author}</p>
-        <a href='/'>Comments: {post.commentCount}</a>
-        <p><span>UP </span><span> DOWN</span></p>
+      <article className='post'>
+        <a href={'/' + post.category} className='post-category'>{post.category}</a>
+        <h2>{post.title}</h2>
+        <p className='post-author'><FaRegUser/> {post.author} {post.timestamp}</p>
+        <p className='post-body'>{post.body}</p>
+        <div className='post-footer'>
+          <div className='post-footer-icons'>
+            <div className='post-comments'>
+              <span><FaRegCommentDots/></span>
+              <div>{post.commentCount}</div>
+            </div>
+            <button><FaRegThumbsUp/></button>
+            <button><FaRegThumbsDown/></button>
+          </div>
+          <a href={'/' + post.category + '/' + post.id} className='post-more'>Read More</a>
+        </div>
       </article>
     )
   }
 }
 
+/*
+<a href={'/' + post.category + '/' + post.id} className='post-comments'><FaRegCommentDots/> {post.commentCount}</a>
+*/
 export default Post
