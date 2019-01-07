293532fc464fe6b2fee5bc0af9dbd8be7cc67ad6
nadjayanna <nadjayannabr@gmail.com>
2019-01-07 19:19:53 +0100

    feat: create component from post to use on the post page

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,34 +1,19 @@
 import React, { Component, } from 'react'
 import { FaThumbsUp, FaRegThumbsUp, FaThumbsDown, FaRegThumbsDown, FaRegUser, FaRegComment, FaRegCommentDots } from 'react-icons/fa';
-import { timeAgo } from '../utils/helper'
 
 class Post extends Component {
   render() {
 
     const { post } = this.props;
+
     return (
       <article className='post'>
-        <a href={'/' + post.category} className='post-category'>{post.category}</a>
         <h2>{post.title}</h2>
-        <p className='post-author'><FaRegUser/> {post.author} {timeAgo(post.timestamp)}</p>
-        <p className='post-body'>{post.body}</p>
-        <div className='post-footer'>
-          <div className='post-footer-icons'>
-            <div className='post-comments'>
-              <span><FaRegCommentDots/></span>
-              <div>{post.commentCount}</div>
-            </div>
-            <button><FaRegThumbsUp/></button>
-            <button><FaRegThumbsDown/></button>
-          </div>
-          <a href={'/' + post.category + '/' + post.id} className='post-more'>Read More</a>
-        </div>
+         <p className='author'><FaRegUser/> {post.author} {post.timestamp}</p>
+         <p>{post.body}</p>
       </article>
     )
   }
 }
 
-/*
-<a href={'/' + post.category + '/' + post.id} className='post-comments'><FaRegCommentDots/> {post.commentCount}</a>
-*/
 export default Post
