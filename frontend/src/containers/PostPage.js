b5b570598cd49ea226e51129f808d09bd085c4d3
nadjayanna <nadjayannabr@gmail.com>
2019-01-07 19:57:34 +0100

    feat: add Comments to Post page

--- frontend/src/containers/PostPage.js
+++ frontend/src/containers/PostPage.js
@@ -1,15 +1,16 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux';
 import { handleDetails } from '../actions/post';
+import { handlePostComments } from '../actions/comments';
+
 import Post from '../components/Post';
-
-
-
+import Comments from '../components/Comments'
 class PostPage extends Component {
 
   componentWillMount() {
     const id = this.props.match.params.id;
     this.props.fetchPostDetails(id);
+    this.props.fetchPostComments(id);
   }
 
 /*  componentWillReceiveProps(nextProps) {
@@ -21,21 +22,27 @@ class PostPage extends Component {
 
   render() {
 
-    const { details, error, loading } = this.props;
+    const { details, error, loading, comments } = this.props;
     if(error) {
       return <div>ERROR!</div>
     }
     if(loading){
       return <div>loading...</div>
     }
+    if(details){
+      return(
+        <Fragment>
+          <Post post={details}/>
+          {comments.length > 0
+            ? <Comments comments={comments}/>
+            : <div>No Comments</div>
+          }
+        </Fragment>
+      )
+    }
 
     return (
-      <Fragment>
-        {details
-          ? <Post post={details}/>
-          : <div>No DETAILS</div>
-        }
-      </Fragment>
+      <div>Post Not Found</div>
     )
   }
 }
@@ -43,11 +50,13 @@ class PostPage extends Component {
 const mapStateToProps = state => ({
   details: state.post.details,
   error: state.post.error,
-  loading: state.post.loading
+  loading: state.post.loading,
+  comments: state.comments.comments
 })
 
 const mapDispatchToProps = dispatch => ({
-  fetchPostDetails: id => dispatch(handleDetails(id))
+  fetchPostDetails: id => dispatch(handleDetails(id)),
+  fetchPostComments: id => dispatch(handlePostComments(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostPage)
