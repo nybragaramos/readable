b41f500f1735ee85391fd46a57db428e8a9c0dbc
nadjayanna <nadjayannabr@gmail.com>
2019-01-07 19:13:53 +0100

    feat: create post reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import posts from './posts'
 import categories from './categories'
+import post from './post'
 
 export default combineReducers({
 	categories,
   posts,
+  post,
 })
---
+++ frontend/src/reducers/post.js
@@ -0,0 +1,42 @@
+import { RECEIVE_POST_BEGIN, RECEIVE_POST_SUCCESS, RECEIVE_POST_FAILURE } from '../actions/post';
+
+const initialState = {
+	details: {},
+	error: null,
+  loading: null
+};
+
+export default function post (state = initialState, action) {
+  switch(action.type) {
+  	case RECEIVE_POST_BEGIN:
+      // Mark the state as "loading".
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+
+    case RECEIVE_POST_SUCCESS:
+      // All done: set loading "false".
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        details: action.details
+      };
+
+    case RECEIVE_POST_FAILURE:
+      // The request failed. It's done. So set loading to "false".
+      // Save the error, so we can display it somewhere.
+      // Since it failed, we don't have items to display anymore, so set `items` empty.
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+        details: null
+      };
+
+    default:
+      return state
+  }
+}
