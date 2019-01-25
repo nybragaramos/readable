473f32d2175b70d5b23211d702e595b772e4da01
nadjayanna <nadjayannabr@gmail.com>
2019-01-25 18:54:19 +0100

    feat: create reducer from new post

--- frontend/src/reducers/post.js
+++ frontend/src/reducers/post.js
@@ -1,4 +1,5 @@
-import { RECEIVE_POST_BEGIN, RECEIVE_POST_SUCCESS, RECEIVE_POST_FAILURE } from '../actions/post';
+import { RECEIVE_POST_BEGIN, RECEIVE_POST_SUCCESS, RECEIVE_POST_FAILURE,
+         FETCH_POST_BEGIN, FETCH_POST_SUCCESS, FETCH_POST_FAILURE } from '../actions/post';
 
 const initialState = {
 	details: {},
@@ -26,6 +27,34 @@ export default function post (state = initialState, action) {
       };
 
     case RECEIVE_POST_FAILURE:
+      // The request failed. It's done. So set loading to "false".
+      // Save the error, so we can display it somewhere.
+      // Since it failed, we don't have items to display anymore, so set `items` empty.
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+        details: null
+      };
+
+    case FETCH_POST_BEGIN:
+      // Mark the state as "loading".
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+
+    case FETCH_POST_SUCCESS:
+      // All done: set loading "false".
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        details: action.details
+      };
+
+    case FETCH_POST_FAILURE:
       // The request failed. It's done. So set loading to "false".
       // Save the error, so we can display it somewhere.
       // Since it failed, we don't have items to display anymore, so set `items` empty.
