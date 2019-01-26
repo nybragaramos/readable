41fa2694cde626378f9ec437f66bae0c7cae3567
nadjayanna <nadjayannabr@gmail.com>
2019-01-26 18:07:49 +0100

    refactor: make the NewPost component to add New and Edit Posts

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import { handleNewPost } from '../actions/post';
+import { handleNewPost, handleEditPost } from '../actions/post';
 import { Redirect, withRouter } from 'react-router-dom';
-class NewPost extends Component {
+class Form extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       post: {
+        id: '',
         title: '',
         body: '',
         author: '',
         category: 'react'
       },
-      id: '',
-      redirect: false
-      
+      redirect: false,
+      edit:false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
+  static getDerivedStateFromProps(props, state){
+    if(props.post){
+      if(props.post.id !== state.post.id){
+        console.log("edit");
+        return{
+          post: props.post,
+          edit: true
+        } 
+      } else {
+        return null;
+      }
+    } else {
+      console.log("new");
+      return {edit: false}
+    }
+  }
+
   handleChange(event) {
     const name = event.target.name;
     const value = event.target.value;
     this.setState(prevState => ({
+      
       post: {
           ...prevState.post,
           [name]: value
@@ -34,42 +52,52 @@ class NewPost extends Component {
   }
 
   handleSubmit(event) {
-   /* alert(`
-      title: ${this.state.title} 
-      author: ${this.state.author} 
-      body: ${this.state.body} 
-      category: ${this.state.category}`);*/
-    
-    this.props.fetchPost(this.state.post)
-    .then((value)=>{this.setState({id: value.id, redirect: true})})
-    
     event.preventDefault();
+    if(this.state.edit){
+      this.props.editPost(this.state.post)
+      .then((value)=>{
+      this.setState({
+        redirect: true
+        })
+      })
+    } else {
+      this.props.addPost(this.state.post)
+      .then((value)=>{
+
+        this.setState(prevState => ({
+          post: {
+            ...prevState.post,
+            id: value.id,
+          },
+          redirect: true
+        }))
+      })
+    }
   }
 
   render() {
-    let categories = this.props.categories;
 
     if (this.state.redirect) {
-      return <Redirect to={`/${this.state.post.category}/${this.state.id}`} />
+      return <Redirect to={`/${this.state.post.category}/${this.state.post.id}`} />
     }
-
+    
     return(
     <div className='post-new'>
       <h2>Create a New Post</h2>
       <form className='post-form' onSubmit={this.handleSubmit}>
         <div className='post-form-group'>
           <label className='post-form-element'>Title</label>
-          <input value={this.state.title} type='text' onChange={this.handleChange} name='title' maxLength="150" required />
+          <input value={this.state.post.title} type='text' onChange={this.handleChange} name='title' maxLength="150" required />
         </div>
         <div className='post-form-group'>
           <label className='post-form-element'>Author</label>
-          <input value={this.state.author} type='text' onChange={this.handleChange} name='author' maxLength="30"required />
+          <input value={this.state.post.author} type='text' onChange={this.handleChange} name='author' maxLength="30"required />
         </div>
         <div className='post-form-group'>
           <label className='post-form-element'>Category</label>
-          <select value={this.state.category} onChange={this.handleChange} name='category'>
+          <select value={this.state.post.category} onChange={this.handleChange} name='category'>
             { 
-              categories.map(category => {
+              this.props.categories.map(category => {
                   return (<option value={category.name} key={category.name}>{category.name}</option>)
                 }
               )
@@ -78,7 +106,7 @@ class NewPost extends Component {
         </div>
         <div className='post-form-group'>
           <label className='post-form-element'>Post</label>
-          <textarea value={this.state.value} onChange={this.handleChange} name='body' rows="10" required />
+          <textarea value={this.state.post.body} onChange={this.handleChange} name='body' rows="10" required />
         </div>
         <input type="submit" value="Submit" />
       </form>
@@ -93,7 +121,8 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  fetchPost: (post) => dispatch(handleNewPost(post))
+  editPost: (post) => dispatch(handleEditPost(post)),
+  addPost: (post) => dispatch(handleNewPost(post))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(NewPost))
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Form))
