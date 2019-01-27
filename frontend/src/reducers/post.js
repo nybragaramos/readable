b9df8c7d1abef9a85441a4744d4c9b840af9c6ad
nadjayanna <nadjayannabr@gmail.com>
2019-01-27 10:23:08 +0100

    fix: dont need to update the state after add/edit post

--- frontend/src/reducers/post.js
+++ frontend/src/reducers/post.js
@@ -1,5 +1,4 @@
-import { RECEIVE_POST_BEGIN, RECEIVE_POST_SUCCESS, RECEIVE_POST_FAILURE,
-         ADD_POST_BEGIN, ADD_POST_SUCCESS, ADD_POST_FAILURE } from '../actions/post';
+import { RECEIVE_POST_BEGIN, RECEIVE_POST_SUCCESS, RECEIVE_POST_FAILURE } from '../actions/post';
 
 const initialState = {
 	details: {},
@@ -36,35 +35,6 @@ export default function post (state = initialState, action) {
         error: action.error,
         details: null
       };
-
-    case ADD_POST_BEGIN:
-      // Mark the state as "loading".
-      return {
-        ...state,
-        loading: true,
-        error: null
-      };
-
-    case ADD_POST_SUCCESS:
-      // All done: set loading "false".
-      return {
-        ...state,
-        loading: false,
-        error: null,
-        details: action.details
-      };
-
-    case ADD_POST_FAILURE:
-      // The request failed. It's done. So set loading to "false".
-      // Save the error, so we can display it somewhere.
-      // Since it failed, we don't have items to display anymore, so set `items` empty.
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-        details: null
-      };
-
     default:
       return state
   }
